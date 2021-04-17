467cf0e669c5a148f600ef202a9dab363054f27b
christoph <me@christophsturm.com>
2021-04-17 21:59:47 +0200

    unknown routes return 404

--- src/test/kotlin/fundertow/FunderTowTest.kt
+++ src/test/kotlin/fundertow/FunderTowTest.kt
@@ -3,28 +3,38 @@ package fundertow
 import failfast.describe
 import io.undertow.Undertow
 import io.undertow.UndertowOptions
+import io.undertow.server.handlers.PathHandler
 import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
 import strikt.api.expectThat
 import strikt.assertions.isEqualTo
 import java.net.ServerSocket
 
+class UserService : RestService {
+    fun create(user: User): User = User("userId", "userName")
+}
+
+data class User(val id: String?, val name: String)
+
+
 object FunderTowTest {
     @Suppress("BlockingMethodInNonBlockingContext")
     val context = describe(FunderTow::class) {
-        it("exists") {
-            val funderTow = autoClose(FunderTow()) { it.close() }
-            val client = okhttp3.OkHttpClient()
+        val funderTow = autoClose(FunderTow(mapOf("/api/user" to UserService()))) { it.close() }
+        val client = okhttp3.OkHttpClient()
+        it("returns 404 if the route is not found") {
             val response = client.newCall(
-                Request.Builder().url("http://localhost:${funderTow.port}/path").build()
+                Request.Builder().url("http://localhost:${funderTow.port}/unconfigured-url")
+                    .post("""{"name":"sentName"}""".toRequestBody()).build()
             ).execute()
             expectThat(response) {
-                get { code }.isEqualTo(500) // it seems undertow returns 500 without a routing config
+                get { code }.isEqualTo(404)
             }
         }
     }
 }
 
-class FunderTow : AutoCloseable {
+class FunderTow(serviceMapping: Map<String, RestService>) : AutoCloseable {
     val port: Int = findFreePort()
 
     private fun findFreePort(): Int = ServerSocket(0).use {
@@ -35,6 +45,7 @@ class FunderTow : AutoCloseable {
     private val undertow: Undertow = Undertow.builder()
         .setServerOption(UndertowOptions.ENABLE_HTTP2, true)
         .addHttpListener(port, "127.0.0.1")
+        .setHandler(PathHandler())
         .build()
 
     init {
@@ -45,3 +56,5 @@ class FunderTow : AutoCloseable {
     }
 
 }
+
+interface RestService
