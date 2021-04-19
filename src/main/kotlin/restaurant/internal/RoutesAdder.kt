e38bb9c9fb239c435dc9ad06423c3140114890a3
christoph <me@christophsturm.com>
2021-04-19 19:11:04 +0200

    make RestServiceHandler private

--- src/main/kotlin/restaurant/internal/RoutesAdder.kt
+++ src/main/kotlin/restaurant/internal/RoutesAdder.kt
@@ -1,20 +1,37 @@
 package restaurant.internal
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import kotlinx.coroutines.runBlocking
 import restaurant.HttpService
 import restaurant.RestService
+import kotlin.reflect.KFunction
+import kotlin.reflect.full.callSuspend
 import kotlin.reflect.full.functions
+import kotlin.reflect.javaType
 
 class RoutesAdder(private val objectMapper: ObjectMapper) {
     fun routesFor(restService: RestService): Routes {
         val functions = restService::class.functions.associateBy { it.name }
-        val postHandler = RestServiceHandler(
-            restService,
-            objectMapper,
-            functions["create"]
-        )
+        val postHandler = functions["create"]?.let {
+            RestServiceHandler(restService, objectMapper, it)
+        }
         return Routes(postHandler)
     }
 }
 
 data class Routes(val postRoute: HttpService?)
+
+@OptIn(ExperimentalStdlibApi::class)
+private class RestServiceHandler(
+    private val service: RestService, private val objectMapper: ObjectMapper, private val function: KFunction<*>
+) : HttpService {
+    private val parameterType = function.parameters[1].type.javaType as Class<*>
+
+    override fun handle(requestBody: ByteArray, pathVariables: Map<String, String>): ByteArray {
+        val parameter = objectMapper.readValue(requestBody, parameterType)
+        return runBlocking {
+            val result = function.callSuspend(service, parameter)
+            objectMapper.writeValueAsBytes(result)
+        }
+    }
+}
