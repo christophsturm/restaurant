d2e8bb907a2353b86218853fe42dd27fc3e3f23a
Christoph Sturm <me@christophsturm.com>
2021-05-05 12:40:10 +0200

    start testing RestFunction

--- src/test/kotlin/restaurant/internal/RestFunctionTest.kt
+++ src/test/kotlin/restaurant/internal/RestFunctionTest.kt
@@ -1,14 +1,19 @@
 package restaurant.internal
 
 import failfast.describe
+import restaurant.RestService
+import strikt.api.expectThat
+import strikt.assertions.isEqualTo
 
 object RestFunctionTest {
     val context = describe(RestFunction::class) {
         describe("parameter type") {
-            pending("detects parameter type for methods with only one parameter") {
-                class A() {
-
+            it("detects parameter type for methods with a single parameter") {
+                data class Body(val field: String)
+                class A() : RestService {
+                    fun create(body: Body) = Unit
                 }
+                expectThat(RestFunction(A::create, A())).get { parameterType }.isEqualTo(Body::class.java)
             }
         }
     }
