3941ef71ad85c1d45b3ad07c3b995158824c67c0
Christoph Sturm <me@christophsturm.com>
2021-07-03 15:03:13 +0200

    introduce constants for common http status values

---
+++ core/src/main/kotlin/restaurant/HttpStatus.kt
@@ -0,0 +1,11 @@
+package restaurant
+
+object HttpStatus {
+    const val OK_200 = 200
+    const val CREATED_201 = 201
+    const val ACCEPTED_202 = 202
+    const val NO_CONTENT_204 = 204
+    const val BAD_REQUEST_400 = 400
+    const val UNAUTHORIZED_401 = 401
+}
+
