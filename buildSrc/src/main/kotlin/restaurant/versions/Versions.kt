1b4daceafe04f9928f7010c8872a5696ebc84566
Christoph Sturm <me@christophsturm.com>
2021-09-07 23:01:03 +0200

    bump jackson, make versions const

--- buildSrc/src/main/kotlin/restaurant/versions/Versions.kt
+++ buildSrc/src/main/kotlin/restaurant/versions/Versions.kt
@@ -1,10 +1,10 @@
 package restaurant.versions
 
-val failgoodVersion = "0.4.6"
-val striktVersion = "0.32.0"
-val kotlinVersion = "1.5.30"
-val jacksonVersion = "2.12.4"
-val coroutinesVersion = "1.5.1"
-val log4j2Version = "2.14.1"
-val pitestVersion = "1.6.5"
-val undertowVersion = "2.2.10.Final"
+const val failgoodVersion = "0.4.6"
+const val striktVersion = "0.32.0"
+const val kotlinVersion = "1.5.30"
+const val jacksonVersion = "2.12.5"
+const val coroutinesVersion = "1.5.1"
+const val log4j2Version = "2.14.1"
+const val pitestVersion = "1.6.5"
+const val undertowVersion = "2.2.10.Final"
