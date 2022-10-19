694f906d939a5f6c1dce695cbe320fea7bead72d
Christoph Sturm <me@christophsturm.com>
2022-10-19 12:46:12 +0200

    dep bump

--- rest/build.gradle.kts
+++ rest/build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
 
 dependencies {
     api("com.fasterxml.jackson.core:jackson-core:$jacksonVersion")
-    api("com.fasterxml.jackson.core:jackson-databind:$jacksonVersion")
+    api("com.fasterxml.jackson.core:jackson-databind:2.13.4.2")
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
     implementation(platform("org.jetbrains.kotlin:kotlin-bom:$kotlinVersion"))
