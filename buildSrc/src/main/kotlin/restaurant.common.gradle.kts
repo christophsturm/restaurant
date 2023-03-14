583c696331b31d51b566d9c265c2f33016ecfa4d
Christoph Sturm <me@christophsturm.com>
2023-03-14 20:47:44 +0100

    set languageVersion to 1.8 for progressive mode

--- buildSrc/src/main/kotlin/restaurant.common.gradle.kts
+++ buildSrc/src/main/kotlin/restaurant.common.gradle.kts
@@ -22,7 +22,7 @@ tasks {
         kotlinOptions {
             jvmTarget = "11" // need at least jdk 11 for the http11 httpclient anyway
             freeCompilerArgs = listOf("-opt-in=kotlin.RequiresOptIn")
-            languageVersion = "1.7"
+            languageVersion = "1.8"
             apiVersion = "1.7"
         }
     }
@@ -30,7 +30,7 @@ tasks {
 kotlin {
     sourceSets.all {
         languageSettings.apply {
-            languageVersion = "1.7"
+            languageVersion = "1.8"
             progressiveMode = true
         }
     }
