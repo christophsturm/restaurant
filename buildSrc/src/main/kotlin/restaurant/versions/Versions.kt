b9dece3a2e2dcde3490a98e9df8d449db6116168
Christoph Sturm <me@christophsturm.com>
2023-12-23 21:29:22 +0100

    dep bump

--- buildSrc/src/main/kotlin/restaurant/versions/Versions.kt
+++ buildSrc/src/main/kotlin/restaurant/versions/Versions.kt
@@ -3,7 +3,7 @@ package restaurant.versions
 const val failgoodVersion = "0.9.0"
 const val striktVersion = "0.34.1"
 const val kotlinVersion = "1.9.22"
-const val jacksonVersion = "2.15.3"
+const val jacksonVersion = "2.16.0"
 const val coroutinesVersion = "1.7.3"
 const val log4j2Version = "2.22.0"
 const val pitestVersion = "1.9.8"
