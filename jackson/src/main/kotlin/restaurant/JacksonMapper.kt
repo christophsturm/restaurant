2dd19502337b1e1c92ac49a4b0db058d6f36df58
Christoph Sturm <me@christophsturm.com>
2023-12-23 21:29:35 +0100

    fix tests for new jackson version

--- jackson/src/main/kotlin/restaurant/JacksonMapper.kt
+++ jackson/src/main/kotlin/restaurant/JacksonMapper.kt
@@ -1,12 +1,18 @@
 package restaurant
 
 import com.fasterxml.jackson.core.JsonProcessingException
+import com.fasterxml.jackson.core.StreamReadFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import restaurant.HttpStatus.BAD_REQUEST_400
 import restaurant.internal.Mapper
 
-class JacksonMapper(private val jackson: ObjectMapper = jacksonObjectMapper()) : Mapper {
+class JacksonMapper(
+    private val jackson: ObjectMapper = jacksonObjectMapper().configure(
+        StreamReadFeature.INCLUDE_SOURCE_IN_LOCATION.mappedFeature(),
+        true
+    )
+) : Mapper {
     override fun <T : Any> readValue(requestBody: ByteArray, clazz: Class<T>): T =
         try {
             jackson.readValue(requestBody, clazz)
