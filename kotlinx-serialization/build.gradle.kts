f269620d8ebc380ba98c34779887bf81a6a2a02b
Christoph Sturm <me@christophsturm.com>
2023-12-24 09:54:31 +0100

    bump version to 0.0.9, also publish kotlinx-serialization prototype

--- kotlinx-serialization/build.gradle.kts
+++ kotlinx-serialization/build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
     kotlin("jvm")
     id("info.solidsoft.pitest")
     id("restaurant.common")
+    id("restaurant.publish")
     id("org.jetbrains.kotlinx.kover")
     id("org.jmailen.kotlinter")
     kotlin("plugin.serialization") version ("1.9.22")
