4187ecfb85d452d4b423ab6d7c2808c36ce8d3b9
Christoph Sturm <me@christophsturm.com>
2024-04-07 14:37:01 +0200

    make HttpStatus accessible from client

--- client/src/main/kotlin/restaurant/HttpStatus.kt
+++ client/src/main/kotlin/restaurant/HttpStatus.kt
@@ -10,5 +10,6 @@ object HttpStatus {
     const val NOT_FOUND_404 = 404
     const val CONFLICT_409 = 409
     const val TEAPOT_418 = 418
+    const val LOCKED_423 = 423
     const val INTERNAL_SERVER_ERROR_500 = 500
 }
