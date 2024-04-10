59bb7c81d9cb0306cfc5e1e8556b5bd5ad90fa77
Christoph Sturm <me@christophsturm.com>
2024-04-10 20:26:50 +0200

    test:  check that we have all needed transient deps declared

---
+++ server-test/build.gradle.kts
@@ -0,0 +1,25 @@
+@file:Suppress("GradlePackageUpdate") // buggy
+
+import restaurant.versions.coroutinesVersion
+import restaurant.versions.failgoodVersion
+import restaurant.versions.kotlinVersion
+import restaurant.versions.log4j2Version
+import restaurant.versions.striktVersion
+import restaurant.versions.undertowVersion
+
+plugins {
+    kotlin("jvm")
+    id("info.solidsoft.pitest")
+    id("restaurant.common")
+    id("org.jetbrains.kotlinx.kover")
+    id("org.jmailen.kotlinter")
+    id("com.bnorm.power.kotlin-power-assert")
+    id("dev.jacomet.logging-capabilities") version "0.11.1"
+}
+
+dependencies {
+    api(project(":restaurant-core"))
+    testImplementation("dev.failgood:failgood:$failgoodVersion")
+}
+
+
---
+++ server-test/src/test/java/ClassPathTest.kt
@@ -0,0 +1,20 @@
+package restaurant
+
+import failgood.Test
+import failgood.tests
+import restaurant.client.Java11HttpClient
+
+@Test
+class ClassPathTest {
+    val t = tests {
+        val mock = Restaurant {
+            route(Method.GET, "/") { _, _ ->
+                response()
+            }
+        }
+
+        test("client and server work without any other dependencies") {
+            assert(Java11HttpClient().send(mock.baseUrl).isOk)
+        }
+    }
+}
