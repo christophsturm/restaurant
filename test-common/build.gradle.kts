cd2f547090da039dec23c62381c9903327a28d87
Christoph Sturm <me@christophsturm.com>
2024-11-21 11:29:26 +0100

    add kotlin-test-junit5 to make idea show diffs.

--- test-common/build.gradle.kts
+++ test-common/build.gradle.kts
@@ -10,6 +10,7 @@ plugins {
 dependencies {
     api("dev.failgood:failgood:$failgoodVersion")
     api(kotlin("test"))
+    api(kotlin("test-junit5")) // this improves assertEquals output and makes idea show a diff
     implementation("ch.qos.logback:logback-classic:1.5.12")
 }
 
